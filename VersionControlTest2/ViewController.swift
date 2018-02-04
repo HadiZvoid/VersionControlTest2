ef0115fdac1929c8f953c4e02fc8d2b71336e07b
Abdulhadi <abdulhadi@Abdulhadis-iMac.local>
2018-02-04 15:43:20 +0200

    Initial Commit

---
+++ VersionControlTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VersionControlTest2
+//
+//  Created by Abdulhadi on 04/02/2018.
+//  Copyright © 2018 Abdulhadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
